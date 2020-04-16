41c2f7faed8d8c6e6a5a680808d0e15e04c41abe
Robie Gonzales <jn912820@dal.ca>
2020-04-16 19:01:31 -0300

    Create 1186A.cpp

---
+++ codeforces/1186A.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+
+	int n, m, k;
+	std::cin >> n >> m >> k;
+
+	if (m < n || k < n) {
+		std::cout << "No";
+	} else {
+		std::cout << "Yes";
+	}
+ 
+  return 0;
+}
